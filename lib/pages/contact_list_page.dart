efd3de50e0b44d1a829a0b67fd6a8c1c2fea3b31
PrantoMondal <prantomondal517@gmail.com>
2022-08-24 01:31:04 +0600

    Pages added

---
+++ lib/pages/contact_list_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ContactListPage extends StatefulWidget {
+  static const String routeName = '/';
+  const ContactListPage({Key? key}) : super(key: key);
+
+  @override
+  State<ContactListPage> createState() => _ContactListPageState();
+}
+
+class _ContactListPageState extends State<ContactListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+
+  }
+}
---
+++ lib/pages/new_contact_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewContactPage extends StatefulWidget {
+  static const String routeName = '/new_contact';
+  const NewContactPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewContactPage> createState() => _NewContactPageState();
+}
+
+class _NewContactPageState extends State<NewContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
