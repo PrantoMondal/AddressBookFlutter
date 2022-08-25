a651748568269d48df2cc93df60789aec7e9d4e4
PrantoMondal <prantomondal517@gmail.com>
2022-08-25 14:33:11 +0600

    contact details page modified

--- lib/pages/contact_details_page.dart
+++ lib/pages/contact_details_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+
+import '../models/contact_model.dart';
 
 class ContactDetailsPage extends StatefulWidget {
   static const String routeName = '/contact_details';
@@ -9,8 +13,26 @@ class ContactDetailsPage extends StatefulWidget {
 }
 
 class _ContactDetailsPageState extends State<ContactDetailsPage> {
+  late ContactModel contact;
+
+  @override
+  void didChangeDependencies() {
+    contact = ModalRoute.of(context)!.settings.arguments as ContactModel;
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(contact.name),
+      ),
+      body: ListView(
+        children: [
+          contact.image == null? Image.asset('images/person.png',width: double.maxFinite,height: 250,fit: BoxFit.cover,) :
+          Image.file(File(contact.image!),width: double.maxFinite,height: 250,fit: BoxFit.cover),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/contact_list_page.dart
+++ lib/pages/contact_list_page.dart
@@ -1,3 +1,5 @@
+import 'package:addressbook/models/contact_model.dart';
+import 'package:addressbook/pages/contact_details_page.dart';
 import 'package:addressbook/pages/new_contact_page.dart';
 import 'package:flutter/material.dart';
 
@@ -16,12 +18,31 @@ class _ContactListPageState extends State<ContactListPage> {
       appBar:AppBar(
         title: const Text('Contact List'),
       ),
+      body: ListView.builder(itemCount: contactList.length,
+      itemBuilder: (context,index){
+        final contact = contactList[index];
+        return Card(
+          elevation: 8,
+          child: ListTile(
+            onTap: () => Navigator
+                .pushNamed(
+                context,
+                ContactDetailsPage.routeName,
+                arguments: contact),
+            leading: CircleAvatar(
+              child: Text(contact.name.substring(0,2).toUpperCase()),
+            ),
+            title: Text(contact.name),
+          ),
+        );
+      },),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, NewContactPage.routeName),
+        onPressed: () => Navigator.pushNamed(context, NewContactPage.routeName).then((value) {
+          setState((){});
+        }),
         child: const Icon(Icons.add),
       ),
     );
-
   }
 }
